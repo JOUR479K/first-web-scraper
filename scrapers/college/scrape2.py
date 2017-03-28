4fc490a3ca4333d011ed3ff5d7787708c02ed1ab
Derek Willis <dwillis@gmail.com>
2017-03-28 11:53:57 -0400

    Update scrape2.py

--- scrapers/college/scrape2.py
+++ scrapers/college/scrape2.py
@@ -1,26 +1,33 @@
 import csv
 import requests
 from BeautifulSoup import BeautifulSoup
+# encoding=utf8  
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
-url = 'https://columbian.gwu.edu/2015-2016'
-response = requests.get(url)
-html = response.content
+years = ['2014-2015', '2015-2016']
 
-soup = BeautifulSoup(html)
-table = soup.find('table')
+for year in years:
+    print year
+    response = requests.get("https://columbian.gwu.edu/" + year)
+    html = response.content
 
-list_of_rows = []
-counter = 1
-for row in table.findAll('tr')[1:-1]:
-    list_of_cells = []
-    for cell in row.findAll('td'):
-        if cell.find('href'):
-            list_of_cells.append("https://columbian.gwu.edu/2015-2016" + cell.find('href')['href'])
-        else:
-            list_of_cells.append(cell.text)
-    list_of_rows.append(list_of_cells)
+    soup = BeautifulSoup(html)
+    table = soup.find('table')
 
-outfile = open("grants1.csv", "wb")
+    list_of_rows = []
+    for row in table.findAll('tr')[1:-1]:
+        list_of_cells = []
+        # i need to put the year into the list of cells
+        list_of_cells.append(year)
+        for cell in row.findAll('td'):
+            # this causes an error - google unicodedecode error
+            list_of_cells.append(cell.text.encode('utf-8')
+        list_of_rows.append(list_of_cells)
+
+
+outfile = open("grants.csv", "wb")
 writer = csv.writer(outfile)
-writer.writerow(["Department", "Faculty", "Sponsor", "Title"])
+writer.writerow(["Year","Department", "Faculty", "Sponsor", "Title"])
 writer.writerows(list_of_rows)
